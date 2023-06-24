e972b014e7918ccb815c7ed61273f58849de2910
Fathur Rahmansyah Maulana Muhammad <90246459+faathuurrr@users.noreply.github.com>
2023-06-24 13:18:06 +0700

    third commit

--- index.php
+++ index.php
@@ -1,5 +1,32 @@
 <?php
-    include ('config.php');
+session_start();
+
+include('config.php');
+
+if (isset($_POST['add_to_cart'])) {
+
+    if (isset($_SESSION['cart'])) {
+        $session_array_id = array_column($_SESSION['cart'], "product_id");
+
+        if (!in_array($_GET['product_id'], $session_array_id)) {
+            $session_array = array(
+                'product_id' => $_GET['product_id'],
+                "product_name" => $_POST['product_name'],
+                "price" => $_POST['price'],
+                "quantity" => $_POST['quantity']
+            );
+            $_SESSION['cart'][] = $session_array;
+        }
+    } else {
+        $session_array = array(
+            'product_id' => $_GET['product_id'],
+            "product_name" => $_POST['product_name'],
+            "price" => $_POST['price'],
+            "quantity" => $_POST['quantity']
+        );
+        $_SESSION['cart'][] = $session_array;
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -10,6 +37,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Brain Coffee</title>
 
+    <link rel=”stylesheet” href=”https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css” />
+
     <!-- font awesome -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css" integrity="sha384-QYIZto+st3yW+o8+5OHfT6S482Zsvz2WfOzpFSXMF9zqeLcFV0/wlZpMtyFcZALm" crossorigin="anonymous">
 
@@ -19,73 +48,75 @@
 
 <body>
     <!-- nav section start -->
-    <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img src="img/logo.png" style="width: 4rem;"></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
-                    <!-- </li>
-                    <form action="about/about.php">
-                        <input type="submit" value="click on me!">
-                    </form> -->
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Chart</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Others
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li class="fw-bold text-center">Operational Hours</li>
-                            <li><a class="dropdown-item" href="#">07.00 AM - 10.00 PM</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li class="fw-bold text-center">Contact</li>
-                            <li><a class="dropdown-item" href="#">@braincoffee.id</a></li>
-                            <li><a class="dropdown-item" href="#">0895-0396-9086</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-dark" type="submit">Search</button>
-                </form>
-            </div>
-        </div>
-    </nav>
+    <?php
+    include('navbar.php');
+    ?>
     <!-- nav section end -->
 
-    <!-- layout section start -->
-    <div class="container-fluid text-center pt-3 pb-3">
-        <div class="row align-items-center">
-            <div class="col-sm-3 text-start">
-                <ul class="fw-bold list-unstyled fs-1">
-                    <li>MINI</li>
-                    <li>CAFFEINE</li>
-                    <li>DEALER</li>
-                </ul>
-                <p style="text-align: justify;">Just a small room for the crowd people's
-                    talk. Like the name, Brain, we build a
-                    comforting space to do brainstorming and
-                    serve somce coffee too for boost the quality
-                    talk. Welcome to our mini caffeine</p>
-                <hr class="border-4 border-dark opacity-100">
-                <p>Wonokromo Tangkis no. 52 Surabaya</p>
-            </div>
-            <div class="col-sm-9 text-end">
-                <img src="img/brain3.png" alt="brain coffee" style="width: 75%;">
-            </div>
+    <div class="offcanvas offcanvas-start" style="width: 30%;" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel"></h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <h1 class="pb-3">item selected</h1>
+            <?php
+            if (!empty($_SESSION['cart'])) {
+            ?>
+                <table class="text-center" border="1">
+                    <tr>
+                        <th>No.</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Subtotal</th>
+                        <th>Action</th>
+                    </tr>
+                    <?php
+                    $no = 1;
+                    foreach ($_SESSION['cart'] as $cart => $val) {
+                        $subtotal = $val['price'] * $val['quantity'];
+                    ?>
+                        <tr>
+                            <td><?php echo $no++; ?>.</td>
+                            <td><?php echo $val['product_name']; ?></td>
+                            <td><?php echo $val['price']; ?></td>
+                            <td><?php echo $val['quantity']; ?></td>
+                            <td><?php echo $subtotal; ?></td>
+                        </tr>
+                    <?php
+                        $grandtotal += $subtotal;
+                    }
+                    if (isset($_GET['action'])) {
+                            unset($_SESSION['cart']);
+                    }
+                    ?>
+                    <tr>
+                        <th colspan="4">Grand Total</th>
+                        <th><?php echo $grandtotal ?></th>
+                        <th>
+                            <a href="index.php?action=clearall">
+                                <button class="btn btn-dark btn-block">Clear all</button>
+                            </a>
+                        </th>
+                    </tr>
+                </table>
+                <div class="pt-4">
+                    <a href="transaksi.php" class="btn btn-dark" style="width: 22rem;">Pay</a>
+                </div>
+                
+            <?php
+            } else {
+                echo "Cart is empty";
+            }
+            ?>
         </div>
     </div>
+
+    <!-- layout section start -->
+    <?php
+    include('layout.php');
+    ?>
     <!-- layout section end -->
 
     <!-- espresso based section start -->
@@ -95,21 +126,28 @@
         </div>
         <div class="row align-items-center text-center flex-nowrap overflow-auto">
             <?php
-                while($product = mysqli_fetch_assoc($product_id)):
+            while ($row = mysqli_fetch_array($product_id)) {
             ?>
-            <div class="col-sm-3 g-2">
-                <div class="card">
-                    <img src="img/espresso.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title"><?= $product['product_name'];?></h5>
-                        <p class="card-text"><?= $product['product_description'];?></p>
-                        <h6 class="card-title">Rp <?= $product['price'];?></h6>
-                        <a href="#" class="btn btn-dark w-100">Add to cart</a>
+                <div class="col-sm-3 g-2">
+                    <div class="card">
+                        <form method="post" action="index.php?product_id=<?= $row['product_id'] ?>">
+                            <img src="img/<?php echo $row['foto'];?>" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title"><?= $row['product_name']; ?></h5>
+                                <p class="card-text"><?= $row['product_description']; ?></p>
+                                <h6 class="card-title">Rp <?= $row['price']; ?></h6>
+                                <input type="hidden" name="product_name" value="<?= $row['product_name'] ?>">
+                                <input type="hidden" name="price" value="<?= $row['price'] ?>">
+                                <div class="pb-3">
+                                    <input type="number" name="quantity" value="1" class="form-control text-center">
+                                </div>
+                                <input type="submit" class="btn btn-dark w-100" name="add_to_cart" value="Add to Cart">
+                            </div>
+                        </form>
                     </div>
                 </div>
-            </div>
             <?php
-                endwhile;
+            }
             ?>
         </div>
     </div>
@@ -121,22 +159,29 @@
             <h1 class="fw-bold">Signature Non Coffee</h1>
         </div>
         <div class="row align-items-center text-center flex-nowrap overflow-auto">
-        <?php
-                while($product = mysqli_fetch_assoc($product_id1)):
+            <?php
+            while ($row = mysqli_fetch_array($product_id1)) {
             ?>
-            <div class="col-sm-3 g-2">
-                <div class="card">
-                    <img src="img/espresso.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title"><?= $product['product_name'];?></h5>
-                        <p class="card-text"><?= $product['product_description'];?></p>
-                        <h6 class="card-title">Rp <?= $product['price'];?></h6>
-                        <a href="#" class="btn btn-dark w-100">Add to cart</a>
+                <div class="col-sm-3 g-2">
+                    <div class="card">
+                        <form method="post" action="index.php?product_id=<?= $row['product_id'] ?>">
+                            <img src="img/<?php echo $row['foto'];?>" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title"><?= $row['product_name']; ?></h5>
+                                <p class="card-text"><?= $row['product_description']; ?></p>
+                                <h6 class="card-title">Rp <?= $row['price']; ?></h6>
+                                <input type="hidden" name="product_name" value="<?= $row['product_name'] ?>">
+                                <input type="hidden" name="price" value="<?= $row['price'] ?>">
+                                <div class="pb-3">
+                                    <input type="number" name="quantity" value="1" class="form-control text-center">
+                                </div>
+                                <input type="submit" class="btn btn-dark w-100" name="add_to_cart" value="Add to Cart">
+                            </div>
+                        </form>
                     </div>
                 </div>
-            </div>
             <?php
-                endwhile;
+            }
             ?>
         </div>
     </div>
@@ -149,21 +194,28 @@
         </div>
         <div class="row align-items-center text-center flex-nowrap overflow-auto">
             <?php
-                while($product = mysqli_fetch_assoc($product_id2)):
+            while ($row = mysqli_fetch_array($product_id2)) {
             ?>
-            <div class="col-sm-3 g-2">
-                <div class="card">
-                    <img src="img/espresso.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title"><?= $product['product_name'];?></h5>
-                        <p class="card-text"><?= $product['product_description'];?></p>
-                        <h6 class="card-title">Rp <?= $product['price'];?></h6>
-                        <a href="#" class="btn btn-dark w-100">Add to cart</a>
+                <div class="col-sm-3 g-2">
+                    <div class="card">
+                        <form method="post" action="index.php?product_id=<?= $row['product_id'] ?>">
+                            <img src="img/<?php echo $row['foto'];?>" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title"><?= $row['product_name']; ?></h5>
+                                <p class="card-text"><?= $row['product_description']; ?></p>
+                                <h6 class="card-title">Rp <?= $row['price']; ?></h6>
+                                <input type="hidden" name="product_name" value="<?= $row['product_name'] ?>">
+                                <input type="hidden" name="price" value="<?= $row['price'] ?>">
+                                <div class="pb-3">
+                                    <input type="number" name="quantity" value="1" class="form-control text-center">
+                                </div>
+                                <input type="submit" class="btn btn-dark w-100" name="add_to_cart" value="Add to Cart">
+                            </div>
+                        </form>
                     </div>
                 </div>
-            </div>
             <?php
-                endwhile;
+            }
             ?>
         </div>
     </div>
@@ -176,34 +228,36 @@
         </div>
         <div class="row align-items-center text-center flex-nowrap overflow-auto">
             <?php
-                while($product = mysqli_fetch_assoc($product_id3)):
+            while ($row = mysqli_fetch_array($product_id3)) {
             ?>
-            <div class="col-sm-3 g-2">
-                <div class="card">
-                    <img src="img/espresso.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title"><?= $product['product_name'];?></h5>
-                        <p class="card-text"><?= $product['product_description'];?></p>
-                        <h6 class="card-title">Rp <?= $product['price'];?></h6>
-                        <a href="#" class="btn btn-dark w-100">Add to cart</a>
+                <div class="col-sm-3 g-2">
+                    <div class="card">
+                        <form method="post" action="index.php?product_id=<?= $row['product_id'] ?>">
+                            <img src="img/<?php echo $row['foto'];?>" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title"><?= $row['product_name']; ?></h5>
+                                <p class="card-text"><?= $row['product_description']; ?></p>
+                                <h6 class="card-title">Rp <?= $row['price']; ?></h6>
+                                <input type="hidden" name="product_name" value="<?= $row['product_name'] ?>">
+                                <input type="hidden" name="price" value="<?= $row['price'] ?>">
+                                <div class="pb-3">
+                                    <input type="number" name="quantity" value="1" class="form-control text-center">
+                                </div>
+                                <input type="submit" class="btn btn-dark w-100" name="add_to_cart" value="Add to Cart">
+                            </div>
+                        </form>
                     </div>
                 </div>
-            </div>
             <?php
-                endwhile;
+            }
             ?>
         </div>
     </div>
     <!-- snack section end -->
 
-    <!-- Copyright -->
-    <footer class="text-center text-white pt-5">
-        <div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-            © 2020 Copyright:
-            <a class="text-dark" href="#">Brain Coffee</a>
-        </div>
-    </footer>
-    <!-- Copyright -->
+    <?php
+    include('footer.php');
+    ?>
 
     <!-- bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
